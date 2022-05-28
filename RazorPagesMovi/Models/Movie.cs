c70a9ac7ce662e7f66ebe74145565bb0b2f96522
RosiAvramova <rosi.avramova@gmail.com>
2022-05-28 13:25:43 +0300

    Part 3 - scaffolding

--- RazorPagesMovi/Models/Movie.cs
+++ RazorPagesMovi/Models/Movie.cs
@@ -4,7 +4,7 @@ namespace RazorPagesMovie.Models
 {
     public class Movie
     {
-        public int ID { get; set; } 
+        public int ID { get; set ; } 
         public string Title { get; set; } = String.Empty;
 
         [DataType(DataType.Date)]
