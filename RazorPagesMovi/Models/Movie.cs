03101cb5bb2df7a9bab240ad4a8dcf0df90c7d1a
RosiAvramova <rosi.avramova@gmail.com>
2022-05-28 15:33:51 +0300

    Part 4 - work with DataBase

--- RazorPagesMovi/Models/Movie.cs
+++ RazorPagesMovi/Models/Movie.cs
@@ -8,7 +8,7 @@ namespace RazorPagesMovie.Models
         public string Title { get; set; } = String.Empty;
 
         [DataType(DataType.Date)]
-        public DateTime ReaseDate { get; set; }
+        public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; } = String.Empty;
         public decimal Price { get; set; }
     }
